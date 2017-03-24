e681c0ac4c2670f0a7a0df316026f21db1bb2310
quelleck <ethan.seyl@gmail.com>
2017-03-24 15:25:29 -0500

    remove file

--- start_checkout.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# Script triggered by UDEV
-# Hudl
-#
-# Created by Ethan Seyl 2016
-#
-export DISPLAY=:0.0; export XAUTHORITY='/var/run/lightdm/root/:0'; sudo /home/pi/DeviceNanny/usb_checkout.py
