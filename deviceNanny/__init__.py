443d3dcf50d2addf68c9857a7148b13873148a42
Josh Lesch <josh.r.lesch@gmail.com>
2018-10-15 10:25:14 -0500

    add env file

--- deviceNanny/__init__.py
+++ deviceNanny/__init__.py
@@ -2,13 +2,15 @@ import os
 
 from flask import Flask
 
+from deviceNanny.config import Config
+
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',  # TODO: add to configs file
         DATABASE=os.path.join(app.instance_path, 'deviceNanny.sqlite')
     )
+    app.config.from_object(Config)
 
     if test_config is None:
         app.config.from_pyfile('config.py', silent=True)
