6c7020177b5e6d668a476df0a8a1d3e5bb92901f
Ethan Seyl <ethan.seyl@gmail.com>
2018-10-16 13:05:03 -0500

    test

--- deviceNanny/db_actions.py
+++ deviceNanny/db_actions.py
@@ -222,8 +222,9 @@ def get_device_status(device_id):
     :param device_id:
     :return: device_name, checked_out_by, time_checked_out, last_reminded
     """
-    device_status = db_fetch("SELECT device_name, checked_out_by, time_checked_out, last_reminded,"
-                             "location FROM devices WHERE device_id = {}".format(device_id))
+    # device_status = db_fetch("SELECT device_name, checked_out_by, time_checked_out, last_reminded,"
+    #                          "location FROM devices WHERE device_id = {}".format(device_id))
+    device_status = db_fetch("SELECT device_name, checked_out_by, time_checked_out, last_reminded,location FROM devices WHERE device_id = 0")
     return device_status
 
 
